d8ba917a7952d9ae40bc205b880d0fd3b195542b
mingsong <459781909@qq.com>
2017-09-13 14:15:35 +0800

    add view into tensorboardTest.py

--- tensorflow_cpu/tensorboardTest.py
+++ tensorflow_cpu/tensorboardTest.py
@@ -1,8 +1,8 @@
 __author__ = 'Song'
-#from utils import mnist_reader
+# from utils import mnist_reader
 import numpy as np
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 # def get_fashion_data():
 #     root = 'fashion'
@@ -53,12 +53,30 @@ def add_layer(inputs, d, neural_num, n_layer, activate_function=None):
         else:
             output = activate_function(wx_plus_b)
         tf.summary.histogram(layer_name+'/output', output)
-    return output
+    return output, weight, bias
 
+def display(X, Y, W, b, W2, b2):
+
+    h = 0.02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = np.dot(np.maximum(0, np.dot(np.c_[xx.ravel(), yy.ravel()], W) + b), W2) + b2
+    Z = np.argmax(Z, axis=1)
+    Z = Z.reshape(xx.shape)
+    fig = plt.figure()
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=Y, s=40, marker='o', cmap=plt.cm.Spectral)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    #fig.savefig('spiral_linear.png')
+    plt.show()
 
 def taskgo():
     X, Y, ktype = get_data()
     n, d = X.shape
+
     yy = np.zeros([n, ktype])
     yy[range(n), Y] = 1
     print('=============')
@@ -67,13 +85,15 @@ def taskgo():
     print(yy[195:205])
 
     hidden = 128
+    # xxx = tf.placeholder(tf.float32, [xxn, xxd])
+    # yyy = tf.placeholder(tf.float32, [yyn, yyd])
     with tf.name_scope('inputs'):
         xtrain = tf.placeholder(tf.float32, [None, d], name='x_input')
         ytrain = tf.placeholder(tf.float32, [None, 3], name='y_input')
 
-    l1 = add_layer(xtrain, 2, hidden, 1, activate_function=tf.nn.relu)
+    l1, W1, b1 = add_layer(xtrain, 2, hidden, 1, activate_function=tf.nn.relu)
 
-    predection = add_layer(l1, 128, ktype, 2, activate_function=None)
+    predection, W2, b2 = add_layer(l1, 128, ktype, 2, activate_function=None)
     with tf.name_scope('loss'):
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predection, labels=ytrain))
         tf.summary.scalar('loss', loss)
@@ -93,6 +113,11 @@ def taskgo():
                 print('iterate %d loss %f:' % (i, sess.run(loss, feed_dict={xtrain:X, ytrain:yy})))
                 res = sess.run(merged, feed_dict={xtrain:X, ytrain:yy})
                 writer.add_summary(res, i)
+        l1, W1, b1 = sess.run((l1, W1, b1), feed_dict={xtrain:X})
+        predection, W2, b2 = sess.run((predection, W2, b2), feed_dict={xtrain:X})
+        # print(type(W1), W1.shape)
+
+    display(X, Y, W1, b1, W2, b2)
 
 
 if __name__ == '__main__':
